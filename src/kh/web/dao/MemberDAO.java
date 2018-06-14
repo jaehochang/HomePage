c9bb2db6ed801336944f72c4d5df861b77d78f8c
Unknown <0219alice@gmail.com>
2018-06-14 16:15:22 +0900

    MemberDAO 수정

--- src/kh/web/dao/MemberDAO.java
+++ src/kh/web/dao/MemberDAO.java
@@ -1,5 +1,27 @@
 package kh.web.dao;
 
-public class MemberDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import kh.web.utils.DBUtils;
+
+public class MemberDAO {
+	public boolean isLoginAvailable(String id, String pw) throws Exception{
+		Connection con = DBUtils.getConnection();
+		
+		String sql = "select id,pw from members where id=? and pw=? and isblocked='N'";
+		PreparedStatement pstat = con.prepareStatement(sql);
+		pstat.setString(1, id);
+		pstat.setString(2, pw);
+		
+		ResultSet rs = pstat.executeQuery();
+	
+		boolean result = rs.next();
+		
+		pstat.close();
+		con.close();
+		
+		return result;
+	}
 }
